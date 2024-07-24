3bcbd7834dab65ecc207758cf0353a857c9923d4
LeeWonHee <pigmong02020202@gmail.com>
2024-07-24 21:51:17 +0900

    chatting.js : userCode 인코딩디코딩 URIComponent로 변경 / 채팅방 입장시 방 번호, 이름 입력창 disable / 채팅방 입장시 나머지 visible / 채팅 도착 시 textarea 가장 밑으로 스크롤

--- src/main/resources/static/js/chatting.js
+++ src/main/resources/static/js/chatting.js
@@ -8,14 +8,21 @@ let stompClient
 document.querySelector("#enterRoomBtn").addEventListener('click', async () => {
     roomId = document.querySelector("#roomNumber").value;
     userName = document.querySelector("#userName").value;
-    userCode = btoa(userName);
+    userCode = encodeURIComponent(userName);
 
-    await connect();
+    await connect().then(() => {
+        document.getElementById("roomNumber").disabled = true;
+        document.getElementById("userName").disabled = true;
+        document.getElementById("enterRoomBtn").disabled = true;
+        document.getElementById("messageDiv").style.visibility = "visible";
+        document.getElementById("chattingRoomDiv").style.visibility = "visible";
+    });
 });
 
 // 메시지 전송 버튼
 document.querySelector("#sendMessageBtn").addEventListener('click', async () => {
     await sendMessage();
+    document.getElementById("message").value = "";
 });
 
 // 소켓 연결
@@ -28,12 +35,14 @@ const connect = async () => {
         console.log("start connecting chatting room...");
 
         stompClient.subscribe(`/topic/message/send/${roomId}`, data => {
-            const sender = atob(JSON.parse(data.body).sender);
+            const sender = decodeURIComponent(JSON.parse(data.body).sender);
             const message = JSON.parse(data.body).msg;
 
             showMessage(sender, message);
         });
     });
+
+    console.log("success to connect chatting room " + roomId);
 }
 
 // 메시지 전송
@@ -52,4 +61,6 @@ function showMessage(sender, message) {
     curText += sender + " : " + message + "\n";
     console.log(curText);
     document.getElementById("chattingRoomTextarea").value = curText;
+    const textarea = document.getElementById("chattingRoomTextarea");
+    textarea.scrollTop = textarea.scrollHeight;
 }
