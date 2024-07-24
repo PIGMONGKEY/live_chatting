a54e4a1b50c16ed215f7ea1a6076476b6a94fbfb
LeeWonHee <pigmong02020202@gmail.com>
2024-07-24 18:11:47 +0900

    initial commit

---
+++ src/main/java/com/example/live_chatting/LiveChattingApplication.java
@@ -0,0 +1,13 @@
+package com.example.live_chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/live_chatting/LiveChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.live_chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
