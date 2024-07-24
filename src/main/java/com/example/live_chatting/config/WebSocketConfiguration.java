f95beadf7c4e1fbfdd0d1b060dcfeea46efca8c8
LeeWonHee <pigmong02020202@gmail.com>
2024-07-24 18:33:02 +0900

    WebSocketConfiguration : WebSocket 설정 완료

---
+++ src/main/java/com/example/live_chatting/config/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.live_chatting.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // subscribe 메시지 접두사 설정
+        registry.enableSimpleBroker("/topic");
+        // 클라이언트가 이 경로로 보내면 브로커가 처리하도록 함
+        registry.setUserDestinationPrefix("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 웹 소켓 엔드포인트 설정
+        registry.addEndpoint("/chatting")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
