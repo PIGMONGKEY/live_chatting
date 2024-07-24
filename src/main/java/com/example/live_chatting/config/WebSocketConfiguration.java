0d8cf72cbb0c13005f4b3e6b62cfc6a32a5b26d9
LeeWonHee <pigmong02020202@gmail.com>
2024-07-24 20:39:29 +0900

    WebSocketConfiguration : 어노테이션 추가 / UserDestination -> ApplicationDestination 수정

--- src/main/java/com/example/live_chatting/config/WebSocketConfiguration.java
+++ src/main/java/com/example/live_chatting/config/WebSocketConfiguration.java
@@ -1,16 +1,20 @@
 package com.example.live_chatting.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // subscribe 메시지 접두사 설정
         registry.enableSimpleBroker("/topic");
         // 클라이언트가 이 경로로 보내면 브로커가 처리하도록 함
-        registry.setUserDestinationPrefix("/app");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
